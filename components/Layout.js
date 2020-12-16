043d0d27ce7f3a884e1ce2562aaa58c557b9ac04
ishant-k <74232790+ishant-k@users.noreply.github.com>
2020-12-16 14:12:05 +0530

    met tag added

--- components/Layout.js
+++ components/Layout.js
@@ -5,11 +5,12 @@ const Layout = (props) => (
     <div>
         <Head>
             <title>BitzPrice-IK</title>
+            <meta name="google-site-verification" content="2rEb2Fi3D2UcWy8buxLnvPVecn8m0ZiO0k3ilNf4oM4" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta name="theme-color" content="#000000" />
             <meta
             name="description"
-            content="IK next first app to analyze analytics of spa application in cybage"
+            content="IK next second app to analyze analytics of spa application in cybage"
             />
             <link  rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.min.css"></link>
         </Head>
